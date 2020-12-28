534a9053b315a36adf9a96e815b90cb259d09245
jofiisidkt <73963808+jofiisidkt@users.noreply.github.com>
2020-12-28 22:18:59 +0100

    transfer

--- test.php
+++ test.php
@@ -2,12 +2,26 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>jofis pi</title>
+        <title>jofis php</title>
+        <link rel="stylesheet" type="text/css"
+        href="sytle.css" >
     </head>
-    <body><h2>
-    <?php
-print_r($_POST[testTxt]);
-?></h2>
-    <span id="g100">test</span>
+    <body>
+        <span id="g100">
+            <?php
+                $test = ($_POST[testTxt]);
+                // print "<span id=\"g100\">".$_POST[testTxt]."</span>";
+
+                /*if(preg_match("/^hallo.*php$/ui",$test))
+                {
+                    
+                }*/
+                $fileC = fopen("cCompiler/test.c", "w");
+                fwrite($fileC, $test);
+                exec("cd home/pi/server-test/cCompiler");
+                exec("ggc test.c");
+                passthru("./a.out");
+            ?>
+        </span>
     </body>
 </html>
