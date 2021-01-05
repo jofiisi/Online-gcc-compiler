75b3fb73ac4f29fd39a89cefe921f165e4857b57
jofiisi <73760260+jofiisi@users.noreply.github.com>
2021-01-05 14:46:40 +0100

    c input auto resize test

--- test.php
+++ test.php
@@ -12,17 +12,16 @@
                 $test = $_POST["testTxt"];
                 $fileC = fopen("cCompiler/$Name.c", "w");
                 fwrite($fileC, $test);
-                exec("export PATH=$PATH:/usr/bin");
-                passthru("gcc ./cCompiler/$Name.c -o ./cCompiler/$Name.out  2>&1");
-                passthru("x86_64-w64-mingw32-gcc -o ./cCompiler/$Name.exe ./cCompiler/$Name.c 2>&1");
+                passthru("export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin; gcc ./cCompiler/$Name.c -o ./cCompiler/$Name.out  2>&1");
+                passthru("export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;x86_64-w64-mingw32-gcc -o ./cCompiler/$Name.exe ./cCompiler/$Name.c 2>&1");
                 passthru("./cCompiler/$Name.out 2>&1");
             ?>
         </span>     
                 <br>     
-                <a href="./cCompiler/<?php echo $Name?>.c">$<?php echo $Name?>.c</a>
+                <a href="./cCompiler/<?php echo $Name?>.c"><?php echo $Name?>.c</a>
                 <br>
-                <a href="./cCompiler/<?php echo $Name?>.out">$<?php echo $Name?>.out</a>
+                <a href="./cCompiler/<?php echo $Name?>.out"><?php echo $Name?>.out</a>
                 <br>
-                <a href="./cCompiler/<?php echo $Name?>.exe">$<?php echo $Name?>.exe</a> 
+                <a href="./cCompiler/<?php echo $Name?>.exe"><?php echo $Name?>.exe</a> 
     </body>
 </html>
